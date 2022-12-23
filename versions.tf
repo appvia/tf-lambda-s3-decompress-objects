e1871a50f9e7a053f386d07caf0baf345236e829
Kashif Saadat <kashifsaadat@gmail.com>
2022-12-23 11:08:24 +0000

    Add 'archive' to required terraform providers

--- versions.tf
+++ versions.tf
@@ -2,6 +2,11 @@ terraform {
   required_version = ">= 0.14"
 
   required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = ">= 2.2"
+    }
+
     aws = {
       source  = "hashicorp/aws"
       version = ">= 3.0"
